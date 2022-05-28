06bb9028ee7c1aaebfe2d82d0f1dac6196404eec
Vedant Roy <vroy101@gmail.com>
2022-05-28 04:08:50 -0400

    refactor websocket impl

---
+++ site/app/websocket/handlers.ts
@@ -0,0 +1,35 @@
+import type { WebSocket } from "../lib/uws";
+import { pack } from "msgpackr";
+import { Codes } from "./codes";
+
+// From UWS docs:
+// * Ironically, JavaScript strings are the least performant of all options, to pass or receive to/from C++.
+// * This because we expect UTF-8, which is packed in 8-byte chars. JavaScript strings are UTF-16 internally meaning extra copies and reinterpretation are required.
+// *
+// * That's why all events pass data by ArrayBuffer and not JavaScript strings, as they allow zero-copy data passing.
+const stringToBuffer = (s: string) => Buffer.from(s);
+
+export function republishMessage(
+  ws: WebSocket,
+  postId: string,
+  bytes: ArrayBuffer
+) {
+  // This is purely for readability
+  const opts = {
+    chan: stringToBuffer(`/post/${postId}`),
+    bytes,
+    isBinary: true,
+    compress: false,
+  };
+  ws.publish(opts.chan, opts.bytes, opts.isBinary, opts.compress);
+}
+
+export function handleSubscribe(ws: WebSocket, postId: string) {
+  const opts = {
+    msg: pack({ kind: Codes.Subscribed }),
+    isBinary: true,
+    compress: false,
+  };
+  ws.subscribe(stringToBuffer(`/post/${postId}`));
+  ws.send(opts.msg, opts.isBinary, opts.compress);
+}
---
+++ site/app/websocket/protocol.ts
@@ -0,0 +1,41 @@
+import zod, { z } from "zod";
+
+const Codes = {
+  Subscribe: 0,
+  ActiveHighlight: 1,
+  CursorPosition: 2,
+  // Never received, only sent to client
+  Subscribed: 2,
+} as const;
+
+export { Codes };
+
+const SubscribeMessage = zod.object({
+  kind: z.literal(Codes.Subscribe),
+  postId: z.string(),
+});
+export type SubscribeMessage = zod.infer<typeof SubscribeMessage>;
+
+const ActiveHighlightMessage = zod.object({
+  kind: z.literal(Codes.ActiveHighlight),
+  postId: z.string(),
+  // TODO: In the future, this can be nested message pack
+  // data that is serialized to binary
+  bin: z.any(),
+});
+export type ActiveHighlightMessage = zod.infer<typeof ActiveHighlightMessage>;
+
+const CursorPositionMessage = zod.object({
+  kind: z.literal(Codes.CursorPosition),
+  postId: z.string(),
+  // See comment in `ActiveHighlight`
+  bin: z.any(),
+});
+export type CursorPositionMessage = zod.infer<typeof CursorPositionMessage>;
+
+export const Message = zod.union([
+  SubscribeMessage,
+  ActiveHighlightMessage,
+  CursorPositionMessage,
+]);
+export type Message = zod.infer<typeof Message>;
