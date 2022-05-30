45ae0cde7076ec8704861bfb2075ad016bb7668c
Vedant Roy <vroy101@gmail.com>
2022-05-30 01:56:02 -0400

    add live cursors

--- inject/src/main.tsx
+++ inject/src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import OverlayApp from "./components/overlayApp";
+import { htmlToElement } from "./utils/dom";
+import CursorChat from "./vendor/cursor-chat";
 
 function injectReactApp(app: React.ReactElement, suffix: string) {
   const RAND_UUID = "537e51e0-1389-4382-af6b-f8a95f1ed6a6";
@@ -13,3 +15,12 @@ function injectReactApp(app: React.ReactElement, suffix: string) {
 }
 
 injectReactApp(<OverlayApp />, "overlay");
+
+document.body.appendChild(
+  htmlToElement(`
+<div id="cursor-chat-layer">
+  <input type="text" id="cursor-chat-box">
+</div>`)
+);
+
+new CursorChat("ws://localhost:1234", "");
