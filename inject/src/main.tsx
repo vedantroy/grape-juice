bb140b45d78e6f6f1bcf545d0bd402d4dc33bb6b
Vedant Roy <vroy101@gmail.com>
2022-06-04 03:09:26 -0400

    remove unused utility

--- inject/src/main.tsx
+++ inject/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import OverlayApp from "./components/overlayApp";
-import { htmlToElement } from "./utils/dom";
 
 function injectReactApp(app: React.ReactElement, suffix: string) {
   const RAND_UUID = "537e51e0-1389-4382-af6b-f8a95f1ed6a6";
