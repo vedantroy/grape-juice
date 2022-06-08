62297e73d9385fdf54e761f6ad059cb6bb9fa80c
Vedant Roy <vroy101@gmail.com>
2022-06-08 04:14:58 -0400

    dockerfile works locally

--- site/app/services/env.ts
+++ site/app/services/env.ts
@@ -7,4 +7,4 @@ export const IS_DEV = MODE === "development";
 export const HOST_URL = env.string("HOST_URL");
 export const CURSOR_CHAT_URL = env.string("CURSOR_CHAT_URL");
 export const WEBSOCKET_URL = env.string("WEBSOCKET_URL");
-export const LMDB_PATH = env.string("LMDB_PATH", "/tmp/dev.db");
+export const LMDB_PATH = env.string("LMDB_PATH", "/tmp/lmdb/dev.db");
