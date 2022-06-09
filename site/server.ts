1fa80d048ea41bc7198b0e86ff39df0784f946b0
Vedant Roy <vroy101@gmail.com>
2022-06-09 02:39:00 -0400

    cors-anywhere deps

--- site/server.ts
+++ site/server.ts
@@ -6,13 +6,6 @@ import cors from "cors";
 import { createRequestHandler } from "@remix-run/express";
 import { initWebSocketOnce } from "./app/websocket";
 
-//const path = require("path");
-//const express = require("express");
-// const compression = require("compression");
-// const morgan = require("morgan");
-// const { createRequestHandler } = require("@remix-run/express");
-// const initWebSocketOnce = require("./app/websocket/impl");
-
 const BUILD_DIR = path.join(process.cwd(), "build");
 
 const app = express();
